0a8a4b0cb4fd3e94c5bb685ddc43734da8305315
Matthieu Bernardin <mbernard@redhat.com>
2024-12-19 15:30:01 +0100

    feat: Implement ExtractEventTime, FormatDate & HashPayload helpers

--- internal/processing/utils.go
+++ internal/processing/utils.go
@@ -3,8 +3,10 @@ package processing
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/openshift-assisted/ccx-exporter/internal/domain/entity"
@@ -14,10 +16,37 @@ var (
 	errMissingKey       = errors.New("missing key")
 	errFieldInvalidType = errors.New("field type was not the expected one")
 	errEmptyValue       = errors.New("empty value")
+
+	dateFormat = "<year>-<month>-<day>T<hour>:<minute>:<second>.<milli>Z"
 )
 
 func ExtractEventTime(event entity.Event) (time.Time, error) {
-	return time.Time{}, errors.New("not implemented")
+	ret := time.Time{}
+
+	var dateStr string
+	var err error
+
+	switch event.Name {
+	case eventNameClusterState:
+		dateStr, err = ExtractString(event.Payload, "updated_at")
+	case eventNameEvent:
+		dateStr, err = ExtractString(event.Payload, "event_time")
+	case eventNameInfraEnvState:
+		dateStr, err = ExtractString(event.Payload, "updated_at")
+	default:
+		return ret, fmt.Errorf("unexpected event name: %s", event.Name)
+	}
+
+	if err != nil {
+		return ret, fmt.Errorf("failed to extract date time: %w", err)
+	}
+
+	ret, err = ValidateDate(dateStr)
+	if err != nil {
+		return ret, fmt.Errorf("invalid date %s: %w", dateStr, err)
+	}
+
+	return ret, nil
 }
 
 func ExtractString(payload map[string]interface{}, key string) (string, error) {
@@ -39,12 +68,26 @@ func ExtractString(payload map[string]interface{}, key string) (string, error) {
 }
 
 func ValidateDate(date string) (time.Time, error) {
-	ret, err := time.Parse("2006-01-02T15:04:05.000Z", date)
+	ret, err := time.Parse("2006-01-02T15:04:05.9Z", date)
 	if err != nil {
 		return ret, fmt.Errorf("failed to parse time: %w", err)
 	}
 
 	return ret, nil
+}
+
+func FormatDate(date time.Time) string {
+	replacer := strings.NewReplacer(
+		"<year>", fmt.Sprintf("%04d", date.Year()),
+		"<month>", fmt.Sprintf("%02d", date.Month()),
+		"<day>", fmt.Sprintf("%02d", date.Day()),
+		"<hour>", fmt.Sprintf("%02d", date.Hour()),
+		"<minute>", fmt.Sprintf("%02d", date.Minute()),
+		"<second>", fmt.Sprintf("%02d", date.Second()),
+		"<milli>", fmt.Sprintf("%03d", int(date.Nanosecond()/1000000)),
+	)
+
+	return replacer.Replace(dateFormat)
 }
 
 func CopyPayload(payload map[string]interface{}) map[string]interface{} {
@@ -66,6 +109,25 @@ func HashValue(payload map[string]interface{}, key string) (string, error) {
 	hash := md5.New()
 
 	_, err = hash.Write([]byte(value))
+	if err != nil {
+		return "", fmt.Errorf("failed to hash value: %w", err)
+	}
+
+	hashBytes := hash.Sum(nil)
+	ret := hex.EncodeToString(hashBytes[:])
+
+	return ret, nil
+}
+
+func HashPayload(payload map[string]interface{}) (string, error) {
+	hash := md5.New()
+
+	payloadStr, err := json.Marshal(payload)
+	if err != nil {
+		return "", fmt.Errorf("failed to marshal payload: %w", err)
+	}
+
+	_, err = hash.Write([]byte(payloadStr))
 	if err != nil {
 		return "", fmt.Errorf("failed to hash value: %w", err)
 	}
--- internal/processing/utils_test.go
+++ internal/processing/utils_test.go
@@ -10,6 +10,8 @@ import (
 )
 
 func TestValidateDate(t *testing.T) {
+	t.Parallel()
+
 	type testCase struct {
 		name     string
 		date     string
@@ -45,8 +47,16 @@ func TestValidateDate(t *testing.T) {
 			date: "2024-02-31T02:57:38.485Z",
 		},
 		{
-			name: "2 digits for fractional second",
-			date: "2024-11-21T02:57:38.48Z",
+			name:     "2 digits for fractional second",
+			date:     "2024-11-21T02:57:38.48Z",
+			valid:    true,
+			expected: time.Date(2024, 11, 21, 2, 57, 38, 480000000, time.UTC),
+		},
+		{
+			name:     "5 digits for fractional second",
+			date:     "2024-11-21T02:57:38.48123Z",
+			valid:    true,
+			expected: time.Date(2024, 11, 21, 2, 57, 38, 481230000, time.UTC),
 		},
 	}
 
@@ -62,6 +72,50 @@ func TestValidateDate(t *testing.T) {
 			if c.valid {
 				assert.Equal(t, c.expected, ts)
 			}
+		})
+	}
+}
+
+func TestFormatDate(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		name     string
+		date     time.Time
+		expected string
+	}
+
+	cases := []testCase{
+		{
+			name:     "happy path",
+			date:     time.Date(2024, 11, 21, 2, 57, 38, 485000000, time.UTC),
+			expected: "2024-11-21T02:57:38.485Z",
+		},
+		{
+			name:     "2 digits for fractional seconds",
+			date:     time.Date(2024, 11, 21, 2, 57, 38, 480000000, time.UTC),
+			expected: "2024-11-21T02:57:38.480Z",
+		},
+		{
+			name:     "0 digits for fractional seconds",
+			date:     time.Date(2024, 11, 21, 2, 57, 38, 0o00000000, time.UTC),
+			expected: "2024-11-21T02:57:38.000Z",
+		},
+		{
+			name:     "5 digits for fractional seconds",
+			date:     time.Date(2024, 11, 21, 2, 57, 38, 485870000, time.UTC),
+			expected: "2024-11-21T02:57:38.485Z",
+		},
+	}
+
+	for i := range cases {
+		c := cases[i]
+
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+
+			dateStr := processing.FormatDate(c.date)
+			assert.Equal(t, c.expected, dateStr)
 		})
 	}
 }
