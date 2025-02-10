f3752373a622cbbf78d32180d9744c8cb0e55d23
Matthieu Bernardin <mbernard@redhat.com>
2025-02-10 18:36:27 +0100

    test: Add cluster with no host e2e

---
+++ test/e2e/cluster_nohost_test.go
@@ -0,0 +1,82 @@
+package e2e_test
+
+import (
+	"context"
+	"os"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/openshift-assisted/ccx-exporter/test/e2e"
+)
+
+var _ = Describe("Checking cluster state event with no host", func() {
+	var testConfig e2e.TestConfig
+	var testContext e2e.TestContext
+
+	var ctx context.Context
+
+	BeforeEach(func() {
+		var err error
+		ctx = context.TODO()
+
+		testConfig = e2e.CreateTestConfig("no-host-cluster")
+
+		testContext, err = e2e.CreateTestContext(testConfig, kubeconfig)
+		Expect(err).NotTo(HaveOccurred())
+
+		err = testContext.DeployAll(ctx)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		// Keep all components if the test failed
+		if CurrentSpecReport().Failed() {
+			GinkgoLogr.Info("Test failed", "config", testConfig)
+
+			return
+		}
+
+		err := testContext.Shutdown(ctx)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	When("pushing a cluster state event", func() {
+		BeforeEach(func() {
+			err := testContext.PushFile(ctx, "resources/input/cluster2.kafka.json")
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("should process the event successfully", func() {
+			By("eventually creating a file in s3 (result) with expected output")
+			Eventually(func(g Gomega, ctx context.Context) {
+				objects, err := testContext.ListS3Objects(ctx, testConfig.OutputS3Bucket, "")
+				g.Expect(err).NotTo(HaveOccurred())
+				g.Expect(len(objects)).To(Equal(1))
+				g.Expect(objects[0]).To(ContainSubstring(e2e.S3Path(e2e.EventTypeClusters, e2e.EventDate)))
+
+				// Parsing actual output
+				actualContent, err := testContext.GetS3Object(ctx, objects[0])
+				g.Expect(err).NotTo(HaveOccurred())
+
+				// Parsing expected output
+				expectedContent, err := os.ReadFile("resources/output/cluster2.s3.json")
+				g.Expect(err).NotTo(HaveOccurred())
+
+				// Check it matches
+				g.Expect(actualContent).To(MatchJSON(expectedContent))
+			}).WithContext(ctx).WithTimeout(time.Minute).WithPolling(5 * time.Second).Should(Succeed())
+
+			By("eventually incrementing the data count metrics")
+			Eventually(func(g Gomega, ctx context.Context) {
+				metric, err := testContext.GetMetric(ctx, e2e.DataCountMetricFamily, e2e.KeyValue{Key: "name", Value: "ClusterState"})
+				g.Expect(err).NotTo(HaveOccurred())
+
+				g.Expect(metric.Counter).NotTo(BeNil())
+				g.Expect(metric.Counter.Value).NotTo(BeNil())
+				g.Expect(*metric.Counter.Value).To(BeEquivalentTo(1))
+			}).WithContext(ctx).WithTimeout(time.Minute).WithPolling(5 * time.Second).Should(Succeed())
+		})
+	})
+})
